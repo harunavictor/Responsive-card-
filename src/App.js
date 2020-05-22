03fffd16406ea9391e6980720cf3ccbbc15a9e44
victor <harunavictor754@gmail.com>
2020-05-22 19:21:01 +0100

    mui with grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+import PplCards from "./Components/PplCards";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    try {
+      const {
+        data: { data },
+      } = await axios.get("https://reqres.in/api/users?page=2");
+      setUsers(data);
+        console.log(data)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Image Gallery with Mui</h2>
+      <Grid container spacing={10} style={{ pading: "2" }}>
+        {users.map((user) => (
+        <Grid item xm={12} ms={6} md={4} lg={4} xl={3}   key={user.id}>
+            <PplCards
+              key={user.id}
+              image={user.avatar}
+               email={user.email}
+              firstName={user.first_name}
+              lastName={user.last_name}
+            />
+          </Grid>
+           ))}
+      </Grid>
     </div>
   );
 }
